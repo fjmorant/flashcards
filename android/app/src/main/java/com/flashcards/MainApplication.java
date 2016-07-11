27361c89e05c4174da3f3b7dbd9c02ee7a5e07a9
Javier Morant <javier.morant@rnfdigital.com>
2016-07-11 07:56:06 +0100

    Upgrade to RN 29

---
+++ android/app/src/main/java/com/flashcards/MainApplication.java
@@ -0,0 +1,37 @@
+package com.flashcards;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnativenavigation.packages.RnnPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        protected boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.asList(
+                    new MainReactPackage(),
+                    new VectorIconsPackage(),
+                    new RnnPackage()
+            );
+        }
+    };
+
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
+    }
+}
