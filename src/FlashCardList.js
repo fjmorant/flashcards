90a8cf832e1a084ba154e8acf4ce3fc692421d6e
Unknown <javier.morant84@gmail.com>
2018-02-01 08:32:49 +0000

    Added flow check

--- src/FlashCardList.js
+++ src/FlashCardList.js
@@ -4,53 +4,56 @@ import {observable, action, computed} from 'mobx'
 import FlashCard from './FlashCard'
 
 export default class FlashCardList {
+  flashCards: {[string]: FlashCard}
+  flashCardRepository: any
 
-    flashCards: any
-    flashCardRepository: any    
+  @observable flashCards = {}
 
-    @observable flashCards = new Map()
+  constructor(flashCardRepository: any) {
+    this.flashCardRepository = flashCardRepository
 
-     constructor(flashCardRepository: any) {
-        this.flashCardRepository = flashCardRepository
+    this.flashCardRepository.retrieve().then(flashCards => {
+      if (flashCards) {
+        const keys = Object.keys(flashCards)
 
-        this.flashCardRepository.retrieve().then((flashCards) => {
-            if (flashCards) {
-                const keys = Object.keys(flashCards)
-                
-                            keys.forEach((key) => {
-                                this.flashCards.set(key, flashCards[key])
-                            })
-            }     
+        keys.forEach(key => {
+          this.flashCards.set(key, flashCards[key])
         })
+      }
+    })
+  }
+
+  @action.bound
+  add(flashCard: FlashCard) {
+    this.flashCards.set(flashCard.id, flashCard)
+
+    this.flashCardRepository.save(this.flashCards)
+  }
+
+  @action.bound
+  delete(flashCardId: string) {
+    this.flashCards.delete(flashCardId)
+
+    this.flashCardRepository.save(this.flashCards)
+  }
+
+  @action.bound
+  edit(flashCard: FlashCard) {
+    this.flashCards.set(flashCard.id, flashCard)
+
+    this.flashCardRepository.save(this.flashCards)
+  }
+
+  getFlashCard = (id: string) => {
+    return this.flashCards.get(id)
+  }
+
+  @computed
+  get list(): Array<FlashCard> {
+    if (!this.flashCards) {
+      return []
     }
 
-    @action.bound add(flashCard: FlashCard) {
-        this.flashCards.set(flashCard.id, flashCard)
-
-        this.flashCardRepository.save(this.flashCards)
-    }
-
-    @action.bound delete(flashCardId: string) {
-        this.flashCards.delete(flashCardId)        
-
-        this.flashCardRepository.save(this.flashCards)        
-    }
-
-    @action.bound edit(flashCard: FlashCard) {
-        this.flashCards.set(flashCard.id, flashCard)
-        
-        this.flashCardRepository.save(this.flashCards)
-    }
-
-    getFlashCard = (id: string) => {
-        return this.flashCards.get(id)        
-    }
-
-    @computed get list() {
-        if (!this.flashCards) {
-            return []
-        }
-
-        return this.flashCards.values()
-    }
+    return this.flashCards.values()
+  }
 }
