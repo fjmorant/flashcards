76f92533be16654ce4c5ea57db3db1aa4af4dcf8
Unknown <javier.morant84@gmail.com>
2018-02-04 17:05:31 +0000

    Fix issue in flashcards list

--- src/FlashCardList.js
+++ src/FlashCardList.js
@@ -7,7 +7,7 @@ export default class FlashCardList {
   flashCards: {[string]: FlashCard}
   flashCardRepository: any
 
-  @observable flashCards = {}
+  @observable flashCards = new Map<string, FlashCard>()
 
   constructor(flashCardRepository: any) {
     this.flashCardRepository = flashCardRepository
