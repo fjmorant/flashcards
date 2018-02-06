f95c063b8592e33098b5a4c0306de73a8d41b130
Unknown <javier.morant84@gmail.com>
2018-02-06 08:23:05 +0000

    Added more tests

--- src/FlashcardsListScreen.test.js
+++ src/FlashcardsListScreen.test.js
@@ -1,11 +1,12 @@
 import 'react-native'
 import React from 'react'
 import FlashcardsListScreen from './FlashcardsListScreen'
+import FlashCard from './FlashCard'
 
 import renderer from 'react-test-renderer'
 
 const flashCardList = {
-  list: [],
+  list: [new FlashCard()],
 }
 
 describe('FlashcardsListScreen', () => {
