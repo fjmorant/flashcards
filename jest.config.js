5dcde5d4a406b4d52ff6af7102e9ea6c11c8a35b
Unknown <javier.morant84@gmail.com>
2018-02-25 20:25:05 +0000

    Fixed tests

--- jest.config.js
+++ jest.config.js
@@ -2,27 +2,32 @@
 /* global module */
 
 module.exports = {
-    preset: 'react-native',
-    moduleNameMapper: {
-        '^[./a-zA-Z0-9$_-]+\\.(bmp|gif|jpg|jpeg|png|psd|svg|webp)$': 'RelativeImageStub',
-        '^React$': '<rootDir>/node_modules/react',
+  preset: 'react-native',
+  moduleNameMapper: {
+    '^[./a-zA-Z0-9$_-]+\\.(bmp|gif|jpg|jpeg|png|psd|svg|webp)$':
+      'RelativeImageStub',
+    '^React$': '<rootDir>/node_modules/react',
+  },
+  modulePathIgnorePatterns: [
+    '<rootDir>/node_modules/react-native/Libraries/react-native/',
+    '<rootDir>/node_modules/react-native/packager/',
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?@rnfdigital/*|react-native|react-navigation|mobx-react|react-clone-referenced-element)',
+  ],
+  setupFiles: ['<rootDir>/node_modules/react-native/jest/setup.js'],
+  transform: {
+    '^.+\\.jsx?$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  globals: {
+    'ts-jest': {
+      useBabelrc: true,
     },
-    modulePathIgnorePatterns: [
-        '<rootDir>/node_modules/react-native/Libraries/react-native/',
-        '<rootDir>/node_modules/react-native/packager/',
-    ],
-    transformIgnorePatterns: ['node_modules/(?!(jest-)?@rnfdigital/*|react-native|react-clone-referenced-element)'],
-    setupFiles: ['<rootDir>/node_modules/react-native/jest/setup.js'],
-    transform: {
-        '^.+\\.jsx?$': 'babel-jest',
-        '^.+\\.tsx?$': 'ts-jest',
-    },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    globals: {
-        'ts-jest': {
-            useBabelrc: true,
-        },
-    },
-    testEnvironment: 'node'    
+  },
+  testEnvironment: 'node',
+  coverageDirectory: './coverage/',
+  collectCoverage: true,
 }
